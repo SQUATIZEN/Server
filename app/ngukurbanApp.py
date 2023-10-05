843f1f0bbbf5fd8fa16a66ececb218b92d79bb00
zainulabidiinn <zainulabidinn20@gmail.com>
2023-10-05 13:05:59 +0700

    first commit

--- app/ngukurbanApp.py
+++ app/ngukurbanApp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f"Container ID: {socket.gethostname()}"
+    return "Aplikasi Ngukurban"
 
 if __name__ == '__main__':
     app.run()
