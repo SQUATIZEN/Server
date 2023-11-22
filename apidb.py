d0f91cc1323fea89a014e14bced85486ed4fd4a8
zainulabidiinn <zainulabidinn20@gmail.com>
2023-11-22 15:26:56 +0700

    commit 5

--- apidb.py
+++ apidb.py
@@ -4,31 +4,32 @@ import mysql.connector
 app = Flask(__name__)
 
 # Konfigurasi database
-db_config = {
-    'host': 'mariadb',
+db = {
+    'host': '127.0.0.1',
     'user': 'root',
-    'password': 'password',
+    'password': 'root',
+    'port': '3306',
     'database': 'ngukurban'
 }
 
 # Membuat koneksi ke database
 def create_db_connection():
-    connection = mysql.connector.connect(**db_config)
+    connection = mysql.connector.connect(**db)
     return connection
 
-# Endpoint dengan metode GET
+# Endpoint untuk mendapatkan semua data
 @app.route('/data', methods=['GET'])
-def get_data():
+def get_all_data():
     # Membuat koneksi ke database
     connection = create_db_connection()
 
     # Membuat kursor untuk menjalankan query
     cursor = connection.cursor()
 
-    # Menjalankan query SELECT untuk mengambil data dari database
+    # Menjalankan query SELECT untuk mengambil semua data dari tabel
     query = "SELECT * FROM ban"
     cursor.execute(query)
-    
+
     # Mengambil hasil query
     result = cursor.fetchall()
 
@@ -39,8 +40,7 @@ def get_data():
     # Mengembalikan respons JSON dengan data yang diperoleh
     return jsonify(result)
 
-
-# Endpoint dengan metode POST
+# Endpoint untuk menambahkan data baru
 @app.route('/data', methods=['POST'])
 def add_data():
     # Mengambil data dari permintaan POST
@@ -52,7 +52,7 @@ def add_data():
     # Membuat kursor untuk menjalankan query
     cursor = connection.cursor()
 
-    # Menjalankan query INSERT untuk menyimpan data ke database
+    # Menjalankan query INSERT untuk menyimpan data baru ke dalam tabel
     query = "INSERT INTO ban (id_ban, id_user, foto_ban) VALUES (%s, %s, %s)"
     values = (data['id_ban'], data['id_user'], data['foto_ban'])
     cursor.execute(query, values)
@@ -63,8 +63,15 @@ def add_data():
     connection.close()
 
     # Mengembalikan respons JSON dengan pesan sukses
-    return jsonify({'message': 'Data berhasil ditambahkan'})
+    return jsonify({'message': 'Data added successfully'})
 
+@app.route('/', methods=['GET'])
+def hello():
+    return jsonify(status="running")
+
+@app.route('/about')
+def about():
+    return 'This is the about page of the application.'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3307)
